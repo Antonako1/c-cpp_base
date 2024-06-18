fb29c5d606eb8ab474e0e1f7fdf820e5fd2eae6c
antonako <114558368+Antonako1@users.noreply.github.com>
2024-06-18 12:37:53 +0300

    Update run.bat

--- win/run.bat
+++ win/run.bat
@@ -3,8 +3,6 @@ IF NOT pwd==%cd% (cd /d %~dp0)
 call .\batch\globals.bat
 cd ..
 cd ..\build
-copy ..\test\test.bat .\win\test.bat
 .\win\%PROJECTNAME%.exe %*
 echo %ERRORLEVEL%
-copy .\win\*.sh ..\test
 cd ..
